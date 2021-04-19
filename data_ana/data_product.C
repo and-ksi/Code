230bed51b9c885dbc661f4ecc64b5f9a8adff833
KINGANDll <kingand168@outlook.com>
2021-04-19 11:41:33 +0800

    shm_open

--- data_ana/data_product.C
+++ data_ana/data_product.C
@@ -1,10 +1,85 @@
-#include <sys/time.h>
-#include <stdio.h>
 #include <math.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/time.h>
 
-double float sinfun(){
-    double float
+#define PI (acos(-1))
+#define CHANNEL_NUM (8)
+
+typedef struct board_head
+{
+    char board_type[8];
+    char board_addr[8];
+    char Ftype[2];
+    char Error[14];
+} BOARD_HEAD;
+
+typedef struct frame_head
+{
+    char channle_id[8];
+    char error[6];
+    char Ftype[2];
+    char length[16];
+    //char timestamp_H[32];
+    //char timestamp_L[32];
+    char timestamp[64];
+} FRAME_HEAD;
+
+BOARD_HEAD board_head;
+FRAME_HEAD frame_head;
+pthread_t ptd[CHANNEL_NUM];
+int channel_id;
+bool interrupt = 0;
+
+float sinfun(float x, int a){
+    float f = 5*sin(PI*2e6*x + a);
+    return f;
 }
+
+void *channel_generate(){
+    pthread_detach(pthread_self());
+    int id = channel_id;
+    channel_id++;
+    if(channel_id < CHANNEL_NUM){
+        pthread_create(&ptd[channel_id], 0, (void *(*)(void *))channel_generate, NULL);
+    }
+
+    sprintf(frame_head.channle_id, "%d", id);
+    memcpy(frame_head.error, "NULL", 4);
+    memcpy(frame_head.Ftype, "00", 2);
+
+    while (interrupt == 0)
+    {
+        
+    }
+    
+    
+}
+
+void data_generate(){
+    memset(&board_head, '\0', sizeof(board_head));
+    memset(&frame_head, '\0', sizeof(frame_head));
+
+    memcpy(board_head.board_addr, "99999999", 8);
+    channel_id = 0;
+
+    pthread_create(&ptd[channel_id], 0, (void *(*)(void *))channel_generate, NULL);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 int main(){
 
---
+++ test/test.C
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct board_head
+{
+    char board_type[8];
+    char board_addr[8];
+    char Ftype[2];
+    char Error[14];
+} BOARD_HEAD;
+
+int main(){
+    BOARD_HEAD board_head;
+    memset(&board_head, '0', sizeof(board_head));
+    memset(&board_head.board_type, '1', 8);
+    memset(&board_head.board_addr, '2', 8);
+    memset(&board_head.Ftype, '3', 2);
+    memset(&board_head.Error, '5', 14);
+
+    char buf[10];
+    memset(&buf, '1', 10);
+    char ll[32];
+    memset(&ll, '0', 32);
+    memcpy(&ll, (const void *)&board_head, 32);
+    int a = 39;
+    sprintf(buf, "%d", a);
+    printf("Struct body: %s\n", ll);
+    printf("Buf : %s\n", buf);
+    putchar(buf[3]);
+    return 0;
+}
+//  1.struct是按照字符顺序存储和读取的
+//  2.向数组中注入数字会自动在后边加入结束字符
