cbe0e6fc4adc77293b786a9505744f72cb071182
KINGANDll <kingand168@outlook.com>
2021-05-20 21:02:54 +0800

    new

---
+++ data_analys/ana.h
@@ -0,0 +1,382 @@
+#ifndef _ANA_H_
+#define _ANA_H_
+
+#include <arpa/inet.h>
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <getopt.h>
+#include <math.h>
+#include <memory.h>
+#include <semaphore.h>
+#include <stdint.h>
+#include <stdio.h>
+#define __USE_GNU
+#include <pthread.h>
+#include <sched.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/resource.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+
+#define DEVICE_NAME_H2C_0 "/dev/xdma0_h2c_0"
+#define DEVICE_NAME_C2H_0 "/dev/xdma0_c2h_0"
+#define DEVICE_NAME_H2C_1 "/dev/xdma0_h2c_1"
+#define DEVICE_NAME_C2H_1 "/dev/xdma0_c2h_1"
+
+#define MAP_SIZE (0x7FFFFFFF) //8bit 2GB    ;max value of int
+#define MAP_BYPASS_SIZE (4 * 1024)
+#define IMG_RAM_POS (0)
+#define RX_SIZE (5 * 1024 * 1024) //1byte 8bit
+
+#define PACK_SIZE (RX_SIZE / 16)
+
+//根据CPU创建和分配线程
+void ptd_create(pthread_t *arg, int k, void *functionbody)
+{
+    int ret;
+
+    pthread_attr_t attr;
+
+    ret = pthread_attr_init(&attr); //初始化线程属性变量,成功返回0,失败-1
+    if (ret < 0)
+    {
+        perror("Init attr fail");
+        exit(1);
+    }
+
+    if (k != -1)
+    { //k为-1时不使用核心亲和属性
+        cpu_set_t cpusetinfo;
+        CPU_ZERO(&cpusetinfo);
+        CPU_SET(k, &cpusetinfo); //将core1加入到cpu集中,同理可以将其他的cpu加入
+
+        ret = pthread_attr_setaffinity_np(&attr, sizeof(cpusetinfo), &cpusetinfo);
+        if (ret < 0)
+        {
+            perror("Core set fail");
+            exit(1);
+        }
+    }
+
+    /* ret = pthread_attr_setscope(&attr, PTHREAD_SCOPE_PROCESS);//PTHREAD_SCOPE_SYSTEM绑定;PTHREAD_SCOPE_PROCESS非绑定
+	if(ret < 0) {
+		perror("Setscope fail");
+		exit(1);
+	} */
+    /* ret = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED); //线程分离属性:PTHREAD_CREATE_JOINABLE（非分离）
+    if (ret < 0)
+    {
+        perror("Detached fail");
+        exit(1);
+    } */
+
+    pthread_create(arg, &attr, (void *(*)(void *))functionbody, NULL);
+    //printf("Id为%d的线程已创建完毕。", *arg);
+
+    pthread_attr_destroy(&attr); //销除线程属性
+}
+
+//speed test pthread
+void *speed_test(void *count, char *order)
+{
+    long *__cnt = (long *)count; //注释掉,使用global identifier代替
+    struct timeval systime;
+    long time0, time1, time2 = 0, cnt1 = 0;
+    double speed0, speed1;
+    int size_of_every_time = 1024; //1024kB
+    gettimeofday(&systime, 0);
+    time0 = systime.tv_sec * 1e6 + systime.tv_usec;
+    while (1)
+    {
+        gettimeofday(&systime, 0);
+        time1 = systime.tv_sec * 1e6 + systime.tv_usec - time0;
+        while (time1 - time2 > 1)
+        {
+            if (*order != 'n')
+            {
+                //calculate every second
+                //speed in one seconds
+                speed0 = (double)(*__cnt - cnt1) /
+                         (double)((time1 - time2) * 1e6 * size_of_every_time);
+                //total average speed
+                speed1 = (double)(*__cnt) / (double)(time1 * 1e6 * size_of_every_time);
+                printf("Average speed: %lfMB/s, second speed: %lfMB/s\n",
+                       speed0, speed1);
+            }
+            time2 = time1;
+            cnt1 = *__cnt;
+        }
+    }
+}
+
+/*开中断*/
+int open_event(char *devicename)
+{
+    int fd;
+    fd = open(devicename, O_RDWR | O_SYNC);
+    if (fd == -1)
+    {
+        printf("open event error\n");
+        return -1;
+    }
+    printf("open event successful!\n");
+    return fd;
+}
+
+/*获取用户中断*/
+int read_event(int fd)
+{
+    int val;
+    //printf("Reading\n");
+    read(fd, &val, 4);
+    //printf("event=%x\n",val);
+    return val;
+}
+/*open bypass devies*/
+static int open_control(char *filename)
+{
+    int fd;
+    fd = open(filename, O_RDWR | O_SYNC);
+    if (fd == -1)
+    {
+        printf("open control error\n");
+        return -1;
+    }
+    return fd;
+}
+
+//map bypass devies addrs
+static void *mmap_control(int fd, long mapsize)
+{
+    void *vir_addr;
+    vir_addr = mmap(0, mapsize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    return vir_addr;
+}
+
+//write bypass devies
+static void write_control(void *base_addr, int offset, uint32_t val)
+{
+    //uint32_t writeval = htoll(val);
+    *((uint32_t *)((uint32_t *)base_addr + offset)) = val;
+}
+
+//read bypass deives
+static uint32_t read_control(void *base_addr, int offset)
+{
+    uint32_t read_result = *((uint32_t *)((uint32_t *)base_addr + offset));
+    //read_result = ltohl(read_result);
+    return read_result;
+}
+
+long long bit_time_read(unsigned int *in_)
+{
+    long long ret = *(in_ + 1);
+    return ((ret << 32) | *(in_ + 2));
+    /* ret = *(in_ + 1);
+    return (*(in_ + 2) | (ret << 32)); */
+}
+
+unsigned int bit_head_read(unsigned int *in_, char sig_0)
+{
+    if (in_ == NULL)
+    {
+        printf("Bit read error!\n");
+        return 0;
+    }
+    unsigned int ret = 0;
+    int i;
+    switch (sig_0)
+    {
+    case 'T': //Board_type
+        return bit_read(in_, 8, 8);
+        break;
+
+    case 'A': //Board_address
+        return bit_read(in_, 8, 8);
+        break;
+
+    case 'Y': //Board_ftype
+        return bit_read(in_, 8, 8);
+        break;
+
+    case 'E': //Board_error
+        return bit_read(in_, 8, 8);
+        break;
+
+    case 'c':
+
+        return (*in_ & 0x000000ff) - 1 - 4;
+        break;
+
+    case 'e':
+
+        return ((*in_ << 18) >> 26);
+        break;
+
+    case 'F':
+
+        return (*in_ << 16) >> 30;
+        break;
+
+    case 'l':
+        if ((*in_ >> 16) < 5)
+        {
+            for (int i = 5; i < 1024; i++)
+            {
+                if (*(in_ + i) & 0x0fff0000 == 0 || *(in_ + i) & 0x00000fff == 0)
+                {
+                    return i - 1;
+                }
+            }
+        }
+        else
+        {
+            return (*in_ >> 16);
+        }
+        break;
+
+    case 'b':
+        printf("**********BOARD INFO**********\n");
+        printf("Board type:%x \nBoard addr:%x\nBoard Ftype:%x\nBoard Error:%x\n\n",
+               bit_read(in_, 32, 8), bit_read(in_, 24, 8),
+               bit_read(in_, 16, 2), bit_read(in_, 14, 14));
+        return 0;
+        break;
+
+    case 'f':
+        printf("**********FRAME INFO**********\n");
+        printf("channel_id: %02x\nError: %02x\nFtype: %x\nLength: %04d\nTimestamp: %llx\n\n",
+               bit_head_read(in_, 'c'), bit_head_read(in_, 'e'),
+               bit_head_read(in_, 'F'), bit_head_read(in_, 'l'), bit_time_read(in_));
+        return 0;
+        break;
+
+    default:
+        printf("sig_0 error!\n");
+        return 0;
+        break;
+    }
+}
+
+//need adc data int pointer 指向需要读取的unsigned int
+int bit_data_read(unsigned int *in_, char sig_1, int d)
+{
+    if (in_ == NULL)
+    {
+        printf("Bit read error!\n");
+        return 0;
+    }
+    int ret;
+    if (d == 0)
+    {
+        switch (sig_1)
+        {
+        case 'd':
+
+            return ((*in_ & 0x0fff0000) >> 16);
+
+            break;
+
+        case 'c':
+
+            return *in_ & 0xf0000000;
+            break;
+
+        default:
+            return 0;
+            break;
+        }
+    }
+    else
+    {
+        switch (sig_1)
+        {
+        case 'd':
+
+            return *in_ & 0x00000fff;
+            break;
+
+        case 'c':
+
+            return *in_ & 0x0000f000;
+            break;
+
+        default:
+            return 0;
+            break;
+        }
+    }
+}
+
+double bit_float_read(unsigned int *in_, int d)
+{
+    if (in_ == NULL)
+    {
+        printf("Bit read error!\n");
+        return 0;
+    }
+    int ret;
+    ret = bit_data_read(in_, 'd', d);
+    return (5. - (double)ret * (10. / (double)(ldexp(1, 12) - 1)));
+}
+
+void *open_error_log()
+{
+    time_t t;
+    char buf[50];
+    struct tm *lt;
+    time(&t);           //获取Unix时间戳。
+    lt = localtime(&t); //转为时间结构。
+    sprintf(buf, "log/error_%d:%d:%d.log\n", lt->tm_hour, lt->tm_min, lt->tm_sec);
+
+    FILE *error_log = fopen(buf, "w+");
+    if (error_log == NULL)
+    {
+        printf("Error log file open failed!\n");
+        exit(1);
+    }
+    return error_log;
+}
+
+void write_error_log(FILE *_fp, unsigned int *edata)
+{
+    FILE *error_fp = (FILE *)_fp;
+    for (int i = -50; i < 0; i++)
+    {
+        fprintf(error_fp, "%x\n", *(edata + i));
+    }
+    fprintf(error_fp, "\n");
+    for (int i = 0; i < 100; i++)
+    {
+        fprintf(error_fp, "%x\n", *(edata + i));
+    }
+    fclose(_fp);
+}
+
+int find_board_head(unsigned int *in_, int k){
+    for(int i = 0; i < 10; i++){
+        if(*(in_ + i) == 0xf00f){
+            return i;
+        }
+        if(k > 0){
+            if (*(in_ - i) == 0xf00f){
+                return -i;
+            }
+        }
+        if(*(in_ + i) == 0 || *(in_ - i) == 0){
+            return 10;
+        }
+    }
+    return 10;
+}
+
+#endif
---
+++ data_analys/send.c
@@ -0,0 +1,303 @@
+#include "ana.h"
+
+//需要被初始化以及随环境改变的参数
+int client_num = 3;
+int channel_num = 8;
+int port = 10000;
+
+//功能指示
+int work;
+static sem_t sem[4];
+struct sockaddr_in clientaddr[10] = {0};
+
+//PCIE读取 变量
+int c2h_fd[2];
+int h2c_fd;
+int control_fd;
+void *control_base;
+int interrupt_fd;
+int read_end_addr;
+char pData[RX_SIZE / 4];
+
+//socket 变量
+int acfd[8];
+
+//统计变量
+int socket_count;
+
+FILE *error_fp;//错误报告
+
+/*中断处理进程*/
+void *event_process()
+{
+    interrupt_fd = open_event("/dev/xdma0_events_0"); //打开用户中断
+    while (work != -1)
+    {
+        sem_wait(sem + 0);
+        //printf("Start read interrupt !\n");
+        read_event(interrupt_fd);                                //获取用户中断
+        read_end_addr = (int)read_control(control_base, 0x0008); //read interrupt reg
+        write_control(control_base, 0x0000, 0xFFFFFFFF);         //ack interrupt
+        sem_post(sem + 1);
+    }
+
+}
+
+void *rx_process(int id)
+{
+    while (work != -1)
+    {
+        sem_wait(sem + 1);
+        //printf("read_end_addr = %x \n", read_end_addr);
+        lseek(c2h_fd[id], (read_end_addr - (50 - id * 25) * 1024 * 1024), SEEK_SET);
+        read(c2h_fd[id], pData, sizeof(pData)); //10MB
+        /* fwrite(pData[0], 4, RX_SIZE / 4, error_fp);
+        fclose(error_fp);
+        exit(1); */
+        sem_post(sem + 2);
+    }
+}
+
+//open such device func:
+void dev_open_fun()
+{
+    control_fd = open_control("/dev/xdma0_bypass");           //打开bypass字符设备
+    control_base = mmap_control(control_fd, MAP_BYPASS_SIZE); //获取bypass映射的内存地址
+
+    c2h_fd[0] = open(DEVICE_NAME_C2H_0, O_RDONLY | O_NONBLOCK); //打开pcie c2h设备
+    if (c2h_fd[0] == -1)
+    {
+        printf("PCIe c2h device open failed!\n");
+    }
+    else
+        printf("PCIe c2h device open successful!\n");
+
+    mmap(0, MAP_SIZE, PROT_READ, MAP_SHARED, c2h_fd[0], 0);
+
+    printf("c2h0 device Memory map successful!\n");
+
+    /*     c2h_fd[1] = open(DEVICE_NAME_C2H_1, O_RDONLY | O_NONBLOCK); //打开pcie c2h_1设备
+    if (c2h_fd[1] == -1)
+    {
+        printf("PCIe c2h_1 device open failed!\n");
+    }
+    else
+        printf("PCIe c2h_1 device open successful!\n");
+
+    mmap(0, MAP_SIZE, PROT_READ, MAP_SHARED, c2h_fd[1], 0);
+
+    printf("c2h[1] device Memory map successful!\n"); */
+
+    h2c_fd = open(DEVICE_NAME_H2C_0, O_WRONLY | O_NONBLOCK); //打开pcie  h2c设备
+    if (h2c_fd == -1)
+    {
+        printf("PCIe h2c device open failed!\n");
+    }
+    else
+        printf("PCIe h2c device open successful!\n");
+
+    mmap(0, MAP_SIZE, PROT_WRITE, MAP_SHARED, h2c_fd, 0);
+
+    printf("h2c device Memory map successful!\n");
+}
+
+//socket和线程创建函数
+void socket_create()
+{
+    int ret;
+    int on = 1;
+    int i;
+    int socket_fd;
+    socklen_t len;
+
+    char buf[10] = {'\0'};
+
+    struct sockaddr_in localaddr = {0};
+    localaddr.sin_family = AF_INET;
+    localaddr.sin_port = htons(port);
+    localaddr.sin_addr.s_addr = INADDR_ANY;
+
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd < 0)
+    {
+        printf("Socket fail!\n");
+        exit(1);
+    }
+    setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+    ret = bind(socket_fd, (struct sockaddr *)&localaddr, sizeof(localaddr));
+    if (ret < 0)
+    {
+        printf("Bind fail!\n");
+        exit(1);
+    }
+    ret = listen(socket_fd, 10);
+    if (ret < 0)
+    {
+        printf("Listen fail!\n");
+        exit(1);
+    }
+
+    for (i = 0; i < client_num; i++)
+    {
+        printf("等待第%d个客户端连接... %d/%d\n", i + 1, i + 1, client_num);
+        *(acfd + i) = accept(socket_fd, (struct sockaddr *)&clientaddr[i], &len);
+        if (acfd[i] < 0)
+        {
+            perror("Accept fail\n");
+            exit(1);
+        }
+        sprintf(buf, "%d", i);
+        ret = send(*(acfd + i), buf, sizeof(buf), 0);
+        if(ret < 0){
+            sprintf(error_fp, buf);
+            perror("Send failed!");
+            exit(1);
+        }
+        memset(buf, '\0', sizeof(buf));
+        recv(*(acfd + i), buf, 10, 0);
+        if(strcmp(buf, "ok") != 0){
+            printf("Connectde error!\n");
+            sprintf(error_fp, buf);
+            exit(1);
+        }
+        printf("第%d个客户端已连接!\n", i + 1); //希望能够显示连接的客户端地址
+    }
+}
+
+void *data_part_send(){
+    int ret;
+    int location;
+    int board_location[1024];
+    int board_num;
+
+    socket_count = 0;
+
+    while(work != -1){
+        sem_wait(sem + 2);
+
+        board_location[0] = find_board_head(pData, 0);
+        for(board_num = 1; board_num < 1024; board_num++){
+            location = find_board_head(pData + board_location[board_num - 1] + 1024, 1);
+            if(location == 10){
+                board_num--;
+                break;
+            }
+            board_location[board_num] = board_location[board_num - 1] + 1024 + location;
+        }
+        location = board_num / client_num + 1;
+
+        for(int i = 1; i < client_num; i++){
+            ret = send(acfd[i], pData + board_location[i * location],
+             4 * (board_location[i * location + 1] - board_location[(i - 1) * location]), 0);
+            if(ret < 0){
+                printf("Data send failed! socket_count: %d\n", socket_count);
+                fprintf(error_fp, "Data send failed! socket_count: %d\n", socket_count);
+                exit(1);
+            }
+        }
+        socket_count++;
+        sem_post(sem + 0);
+    }
+}
+
+int main(int argc, char const *argv[]){
+    char sig;
+    while(sig != 'y'){
+        printf("Port = %d\nClient_num = %d\nChannel_num = %d\nPress'y'and enter to continue...", port, client_num, channel_num);
+        if(sig != 'y'){
+            printf("port, client_num, channel_num\n");
+            scanf("%d%d%d", &port, &client_num, &channel_num);
+        }
+    }
+
+    unsigned int pos = 0;
+    unsigned int cnontrol_3;
+
+    sem_init(&sem[0], 0, 1);
+    sem_init(&sem[1], 0, 0);
+    sem_init(&sem[2], 0, 0);
+
+    error_fp = open_error_log();
+    work = 1;
+
+    pthread_t rx_thread, event_thread;
+    pthread_t part_send_thread;
+
+    dev_open_fun();
+    socket_create();
+
+    ptd_create(&rx_thread, -1, rx_process);
+    pthread_create(&event_thread, 0, event_process, 0);
+    ptd_create(&part_send_thread, 4, data_part_send);
+
+    while (sig != 'o')
+    {
+        sig = getchar();
+        switch (sig)
+        {
+        case 'w':
+            lseek(c2h_fd[0], 0, SEEK_SET);
+            read(c2h_fd[0], pData, 4 * 1024);
+            printf("Read data successful!\n");
+
+            for (int i = 0; i < 1024; i++)
+            {
+                printf("The data of address %d is %x \n", i, pData[i]);
+            }
+            break;
+
+        case 'e':
+            socket_count = 0;
+            break;
+
+        case 'r':
+            write_control(control_base, 0x0000, 0xFFFFFFFF);
+            break;
+
+        case 't':
+            write_control(control_base, 0x0004, 0xFFFFFFFF);
+            break;
+
+        case 'y':
+            cnontrol_3 = read_control(control_base, 0x0008);
+            printf("read_end_addr = %x \n", cnontrol_3);
+            break;
+
+        case 'u':
+            pos = pos + 1024;
+            printf("test_read_end_addr = %x \n", pos);
+            break;
+
+        case 'i':
+            pos = 0;
+            printf("test_read_end_addr = %x \n", pos);
+            break;
+
+        case 'g':
+            memcpy(pData[1], pData[0], sizeof(pData[1]));
+            FILE *fp;
+            fp = fopen("log/a_data.log", "w+");
+            if (fp == NULL)
+            {
+                printf("File open failed!");
+                break;
+            }
+            fwrite(pData, 1, sizeof(pData[1]), fp);
+            fclose(fp);
+            memset(pData, 0, sizeof(pData[1]));
+            break;
+        }
+    }
+    work = -1;
+
+    close(c2h_fd[0]);
+    //close(c2h_fd[1]);
+    close(h2c_fd);
+    close(control_fd);
+    close(interrupt_fd);
+    for (int i = 0; i < client_num; i++)
+    {
+        close(acfd[i]);
+    }
+    return 0;
+}
