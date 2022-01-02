c34b8219457254e887e64b1734e211aaebcca1b6
KINGANDll <kingand168@outlook.com>
2022-01-02 22:41:44 +0800

    cpp study

---
+++ c++prime/p7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+
+int main()
+{
+    int v1 = 0, v2 = 0;
+    std::cout << "please enter 2 number"<< std::endl;
+    //std::cout << "slkdfj" << std::endl;
+    std::cin >> v1 >> v2;
+    //std::cout << "0002" << std::endl;
+    std::cout << "the sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
+    system("pause");
+    return 0;
+}
