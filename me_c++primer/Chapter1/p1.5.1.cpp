a6e98e15223b8499eac4a7265609efb61b87f1f2
KINGAND <kingand_and@outlook.com>
2022-01-25 23:18:41 +0800

    index changes

---
+++ me_c++primer/Chapter1/p1.5.1.cpp
@@ -0,0 +1,15 @@
+#include "Sales_item.h"
+
+int Item_output()
+{
+    Sales_item book;
+    std::cin >> book;
+    std::cout << book << std::endl;
+    return 0;
+}
+
+int main()
+{
+    Item_output();
+    return 0;
+}
