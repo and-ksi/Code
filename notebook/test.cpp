3a609382b4839998f78562ab1ca0cad4ddc5b292
KINGAND <kingand_and@outlook.com>
2022-06-23 23:04:22 +0800

    Binary Tree Print Success!!!

--- notebook/test.cpp
+++ notebook/test.cpp
@@ -1,14 +1,29 @@
 #include<iostream>
+#include <sstream>
 
 using namespace std;
 
+//测试流操作算子能否在无stream下使用
+//显然不行
+int teststream(){
+    stringstream str;
+    str << "10";
+    string sss;
+    sprintf(&sss[0], "test");
+    cout << sss;
+    return 1;
+}
+
+/*
+ __(45)
+   |
+ (1234)
+
+"     ______(A0)"
+"********", '_' <repeats 13 times>, "(ROOT)"
+*/
+
 int main(){
-    char a, b;
-    int l = 0, m = 0;
-    char k[10];
-    cin >> a;
-    cin >> l;
-    cin >> b;
+    teststream();
     return 0;
 }
-//12f9f8f13f14ef100eeeeee
